b82c86086344098b448c8872393b6fb8f12a7c5a
Ayo-Show <sowunmi30@gmail.com>
2022-01-05 11:29:20 -0500

    Added files to create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khidr", last_name: "Matin", hourly_rate: 100)
+@store1.employees.create(first_name: "Sam", last_name: "Fisher", hourly_rate: 45)
+@store1.employees.create(first_name: "Ethan", last_name: "Cole", hourly_rate: 50)
+@store1.employees.create(first_name: "Joe", last_name: "Rice", hourly_rate: 200)
+@store1.employees.create(first_name: "Brooke", last_name: "Garry", hourly_rate: 150)
